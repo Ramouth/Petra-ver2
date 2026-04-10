700b0f263881668236ab355d5dbdf95a90976e1d
Ramouth <moutiq@gmail.com>
2026-04-10 10:24:54 +0200

    Bug fix in reeval script

--- jobs/reeval_sf.sh
+++ jobs/reeval_sf.sh
@@ -2,7 +2,7 @@
 #BSUB -J reeval_sf
 #BSUB -q hpc
 #BSUB -n 32
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=1GB]"
 #BSUB -W 6:00
 #BSUB -o /zhome/81/b/206091/logs/reeval_sf_%J.out
 #BSUB -e /zhome/81/b/206091/logs/reeval_sf_%J.err
@@ -10,7 +10,7 @@
 source /zhome/81/b/206091/petra-env/bin/activate
 module load gcc/13.4.0-binutils-2.44
 
-python3 /zhome/81/b/206091/Petra-ver2/src/reeval_stockfish.py \
+python3 -u /zhome/81/b/206091/Petra-ver2/src/reeval_stockfish.py \
     --dataset /zhome/81/b/206091/Petra-ver2/data/dataset.pt \
     --out /zhome/81/b/206091/Petra-ver2/data/dataset_sf.pt \
     --stockfish /zhome/81/b/206091/bin/stockfish \
